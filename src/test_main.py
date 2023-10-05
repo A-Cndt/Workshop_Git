56c17c279968f2973caf89cd9034942ebdd20ea7
A-Cndt <101662605+A-Cndt@users.noreply.github.com>
2023-10-05 11:34:57 +0200

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,14 @@
 import main as M 
 
 def test_somme():
+    """
+    Fonction de test pour le main.py
+    ================================
+
+    Ce script va vérifier que la fonction "somme" de deux nombres (a = 1 ; b = 2) retourne effectivement leur somme (3 dans ce test)
+
+    C'est un exemple pour illustrer cette fonctionalité dans le cadre du Workshop Git
+    """
     assert M.somme(1, 2) == 3
 
 test_somme()
